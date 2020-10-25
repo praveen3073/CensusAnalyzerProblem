8754b4f1917927d9a2036eb22d212069e66544fc
praveen3073 <pop3073@gmail.com>
2020-10-25 20:53:36 +0530

    Added: Master Welcome Message

---
+++ main/java/com/censusanalysis/CensusAnalysisMain.java
@@ -0,0 +1,7 @@
+package com.censusanalysis;
+
+public class CensusAnalysisMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyzer");
+    }
+}
