bbcc6e0225ed441246ecdef601d44ca238bddef4
praveen3073 <pop3073@gmail.com>
2020-10-26 09:25:10 +0530

    Added: UC1 Load Census Info From A CSV File

--- main/java/com/censusanalysis/CensusAnalysisMain.java
+++
@@ -1,7 +0,0 @@
-package com.censusanalysis;
-
-public class CensusAnalysisMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Indian States Census Analyzer");
-    }
-}
